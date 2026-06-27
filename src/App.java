eded53154b74da37aeb74e4c21c7e9a6610d812b
magui <lioimagali28@gmail.com>
2026-06-27 17:15:47 -0300

    agrego nuevo ejercicio de java

--- src/App.java
+++ src/App.java
@@ -17,13 +17,34 @@ public class App {
 
 //EJERCCIO PARA PEDIR EDAD AL USUSARIO Y MOSTRAR SU EDAD UN AÑO DESPUES
 
-System.out.println("ingrse su edad: ");
+/*System.out.println("ingrse su edad: ");
 int edad;
 edad= sc.nextInt();
 edad= edad + 1;
-System.out.println("su edad el proximo año sera: " + edad+" años");
+System.out.println("su edad el proximo año sera: " + edad+" años");*/
 
 //FIN DEL EJERCICIO
+
+//APLICACION QUE CALCULE LA MEDIA ARITMETICA DE DOS NOTAS ENTERAS. LA MEDIA PUEDE CONTENER DECIMALES.
+
+int nota1, nota2;
+double media;
+System.out.print("ingrese la primera nota: ");
+nota1= sc.nextInt();
+System.out.print("ingrese la segunda nota: ");
+nota2= sc.nextInt();
+media= (nota1 + nota2) / 2.0;
+System.out.println("la media es: "+ media);
+//FIN DEL EJERCICIO
+
+
+
+
+
+
+
+
+
 
 
 
