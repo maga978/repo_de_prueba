79aefcddf4bdf912109be4ca84a5b0205aa752cb
maga978 <lioimagali28@gmail.com>
2026-07-04 17:55:51 -0300

    Agregar ejerccio de practica

---
+++ Condicionales/condicionales/src/App.java
@@ -0,0 +1,62 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        scanner sc =new Scanner (System.in);
+
+//DISEÑAR UNA APLICACION QUE SOLICITE AL USUARIO UN NUMERO E INDIQUE SI ES PAR O IMPAR
+
+int num;
+System.out.print("introduzca un numero");
+num = sc.nextInt();
+if (num % 2 ==0){
+    System.out.System.err.println("el numero es par");
+}else{
+    System.out.println("el numero e impar");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
