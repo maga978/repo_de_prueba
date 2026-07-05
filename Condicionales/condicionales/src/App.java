5f7a9d6fbec9601e3790ef918f853a84d28e885d
maga978 <lioimagali28@gmail.com>
2026-07-05 15:59:10 -0300

    Update App.java
    limpieza del codigo

--- Condicionales/condicionales/src/App.java
+++ Condicionales/condicionales/src/App.java
@@ -1,19 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        scanner sc =new Scanner (System.in);
+        Scanner sc =new Scanner (System.in);
 
-//DISENAR UNA APLICACION QUE SOLICITE AL USUARIO UN NUMERO E INDIQUE SI ES PAR O IMPAR
 
-         int num;
-        System.out.print("introduzca un numero");
-            num = sc.nextInt();
-            if (num % 2 ==0){
-                System.out.System.err.println("el numero es par");
-            }else{
-                System.out.println("el numero e impar");
-            }
 
+       // DISEÑAR UNA APLICACIÓN QUE SOLICITE AL USUARIO UN NÚMERO E INDIQUE SI ES PAR O IMPAR
+
+int num;
+System.out.print("introduzca un numero: ");
+num = sc.nextInt();
+
+if (num % 2 == 0) {
+    System.out.println("el numero es par");
+} else {
+    System.out.println("el numero es impar");
+}
 
 
 
