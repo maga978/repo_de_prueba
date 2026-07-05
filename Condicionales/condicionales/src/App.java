b4e594350649680ac8563ae9f2ec8f542c47e9ed
maga978 <lioimagali28@gmail.com>
2026-07-05 15:52:41 -0300

    Update App.java

--- Condicionales/condicionales/src/App.java
+++ Condicionales/condicionales/src/App.java
@@ -3,16 +3,16 @@ public class App {
         System.out.println("Hello, World!");
         scanner sc =new Scanner (System.in);
 
-//DISEÑAR UNA APLICACION QUE SOLICITE AL USUARIO UN NUMERO E INDIQUE SI ES PAR O IMPAR
+//DISENAR UNA APLICACION QUE SOLICITE AL USUARIO UN NUMERO E INDIQUE SI ES PAR O IMPAR
 
-int num;
-System.out.print("introduzca un numero");
-num = sc.nextInt();
-if (num % 2 ==0){
-    System.out.System.err.println("el numero es par");
-}else{
-    System.out.println("el numero e impar");
-}
+         int num;
+        System.out.print("introduzca un numero");
+            num = sc.nextInt();
+            if (num % 2 ==0){
+                System.out.System.err.println("el numero es par");
+            }else{
+                System.out.println("el numero e impar");
+            }
 
 
 
