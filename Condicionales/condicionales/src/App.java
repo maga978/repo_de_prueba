7fe9aeff5b1c35cbdb116610d5ea0b96fd1dd458
maga978 <lioimagali28@gmail.com>
2026-07-05 22:45:08 -0300

    se agrego un ejerccio nuevo de if-else

--- Condicionales/condicionales/src/App.java
+++ Condicionales/condicionales/src/App.java
@@ -9,7 +9,7 @@ public class App {
 
        // DISEÑAR UNA APLICACIÓN QUE SOLICITE AL USUARIO UN NÚMERO E INDIQUE SI ES PAR O IMPAR
 
-int num;
+/*int num;
 System.out.print("introduzca un numero: ");
 num = sc.nextInt();
 
@@ -17,9 +17,23 @@ if (num % 2 == 0) {
     System.out.println("el numero es par");
 } else {
     System.out.println("el numero es impar");
-}
+}*/
+//FIN DEL EJERCCIO
 
+//PEDIR DOS NUMEROS ENTEROS Y DECIR SI SON IGUALES
+ /*int num1;
+ int num2;
+ System.out.print("ingrese el primer numero: ");
+ num1=sc.nextInt();
+ System.out.print("ingrese el segundo numero: ");
+ num2=sc.nextInt();
 
+ if(num1==num2){
+System.out.print("los numeros ingresados son iguales: " + num1 +" " + "y" +" "+ num2);
+ }else{
+System.out.print("los numeros ingresados son distintos: " + num1 +" " + "y" +" "+ num2);
+ }*/
+//fin del ejercicio
 
 
 
