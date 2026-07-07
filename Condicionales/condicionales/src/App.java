f4a4e786bbb369200fe4ebc6286262b15fe23dbf
maga978 <lioimagali28@gmail.com>
2026-07-07 20:05:44 -0300

    ejerccio nuevos

--- Condicionales/condicionales/src/App.java
+++ Condicionales/condicionales/src/App.java
@@ -33,9 +33,58 @@ System.out.print("los numeros ingresados son iguales: " + num1 +" " + "y" +" "+ 
  }else{
 System.out.print("los numeros ingresados son distintos: " + num1 +" " + "y" +" "+ num2);
  }*/
+
 //fin del ejercicio
 
+//SOLICITAR DOS NUMEROS DISTINTO Y MOSTRAR CUAL ES EL MAYOR
+/*int num1;
+int num2;
+System.out.print("ingrese el primer numero: ");
+num1=sc.nextInt();
 
+System.out.print("ingrese el segundo numero: ");
+num2=sc.nextInt();
+if(num1 > num2){
+System.out.print("el el primer numero es el mayor"+ num1 + " ");
+}else{
+    System.out.print("el segundo numero es el mayor: " + num2 +" ");
+}*/
+
+//IMPLEMENTAR UN PRGRAMA QUE PIDA UN NUMERO DECIMAL E INDIQUE SI ES U NUMERO CASI CERO
+
+/*double num;
+System.out.print("ingrese un numero: ");
+num=sc.nextDouble();
+if((num>0 && num<1)||(num < 0 && num > -1)){
+System.out.print("el numero es un casi cero");
+}else{
+System.out.print("el numero no entra enn el grupo de casi cero");
+}*/
+
+//PEDIR DOS NUMEROS Y MOSTRARLOS ORDENADOS DE FORMA DECRECIENTE
+
+/*int num1;
+int num2;
+System.out.print("ingrese el  primer numero: ");
+num1=sc.nextInt();
+System.out.print("ingrese el segundo numero: ");
+num2=sc.nextInt();
+if((num1>num2)||(num2<num1)){
+System.out.print("el primer numero es el mayor: "+num1+ " " +"mayor"+ " " +num2+ " " + "menor");
+}else{
+    System.out.print("el segundo numero es el mayor: "+num2+ " " +"mayor"+ " " +num1+" "+"menor");
+}*/
+
+//pedir tres numeros y mostrarlos ordenados de mayor a menor
+int num1;
+int num2;
+int num3;
+ System.out.print("introduzca el primer numero: ");
+ num1=sc.nextInt();
+ System.out.print("introduzca el segundo numero: ");
+ num2=sc.nextInt();
+ System.out.print("introduzca el tercer numero: ");
+ num3=sc.nextInt();
 
 
 
